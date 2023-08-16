c57f7037cfb19ead752c74a827eac626c885399a
김주하 <101048129+laketree2@users.noreply.github.com>
2023-08-16 16:45:55 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,14 @@
+'''
+구호 SONGDO에 대해 HIGHSCHOOL로 응원.
+구호 CODE에 대해 MASTER로 응원.
+구호 2023에 대해 0611로 응원.
+구호 ALGORITHM에 대해 CONTEST로 응원.
+딕셔너리 사용
+'''
+cheer_dict = {
+    "SONGDO" : "HIGHSCHOOL", 
+    "CODE" : "MASTER",
+    "2023" : "0611",
+    "ALGORITHM" : "CONTEST"
+}
+print(cheer_dict[input()])
