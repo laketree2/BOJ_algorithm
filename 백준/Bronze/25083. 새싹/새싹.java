180e39044bd58b8ca2dfa4125ec4b5398ee58271
김주하 <101048129+laketree2@users.noreply.github.com>
2023-08-19 01:51:56 +0900

    [Bronze V] Title: 새싹, Time: 128 ms, Memory: 13816 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'"); //이스케이프 시퀀스
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
