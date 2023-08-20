bf78e9315d9bf41c38db930b1d601a674338e7e8
김주하 <101048129+laketree2@users.noreply.github.com>
2023-08-21 05:48:20 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 312 ms, Memory: 47788 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x: #부모 노드가 같지 않으면
+        parent[x] = find_parent(parent, parent[x]) #부모 노드 통합
+    return parent[x]
+
+def union_parent(parent, a, b): #제일 작은 부모 노드 통합
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+        
+v, e = map(int, input().split())
+parent = [i for i in range(v+1)] #인덱스 1부터 시작, 1 무시
+info = list()
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    info.append((c, a, b)) #가중치를 먼저 내세워 
+info.sort() #가중치 기준으로 정렬(가중치 작은 순)
+
+total = 0 #가중치
+for i in range(e):
+    c, a, b = info[i]
+    if find_parent(parent, a) != find_parent(parent, b): #부모 노드가 다르면
+        union_parent(parent, a, b) #통합
+        total += c #가중치 추가
+        
+print(total)
