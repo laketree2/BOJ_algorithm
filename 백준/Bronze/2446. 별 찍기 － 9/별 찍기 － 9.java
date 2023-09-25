5b2e67ceb8dc6de45d23643ed34346606c0159cc
김주하 <202214022@office.skhu.ac.kr>
2023-09-25 23:06:54 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 448 ms, Memory: 21600 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+ 
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+            
+			for (int k = 0; k < (2 * n - 1) - (2 * i); k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+         //삼각형
+		for (int i = 0; i < n - 1; i++) {	// n+1 ~ 2n-1
+			for (int j = 1; j < (n - 1) - i; j++) {	
+				System.out.print(" ");
+			}
+
+			for (int k = 0; k < 3 + 2 * i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+ 
+}
